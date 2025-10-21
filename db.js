3e0b6c068bc638c52e44e0ce5ad09f9c7f4a0e61
Jorge Miguel A. Brito <99404750+jorgemiguelabrito@users.noreply.github.com>
2025-10-21 11:03:26 -0300

    av2.1
    av2.1

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const { Pool } = require('pg');
 
+// Crie um objeto Pool para gerenciar as conexões com o banco
 const pool = new Pool({
-    connectionString: 'COLE_AQUI_A_SUA_EXTERNAL_DATABASE_URL_DO_RENDER',
+    //
+    // SUA URL COMPLETA FOI COLADA AQUI
+    // 
+    connectionString: 'postgresql://aluguel_de_equipamentos_de_ti_user:LjGQVY0WmyohT8JMBGY4x57PIv2qjTy2@dpg-d3ro5b95pdvs73fsa9cg-a.oregon-postgres.render.com/aluguel_de_equipamentos_de_ti',
+    //
+    
+    // Configuração necessária para conexões SSL com o Render
     ssl: {
         rejectUnauthorized: false
     }
 });
 
+// Exporta o pool para que o arquivo index.js possa usá-lo
 module.exports = { pool };
+
