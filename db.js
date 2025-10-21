37d232c31adf6acbfdf1c4edc4199da1cdc62f31
Jorge Miguel A. Brito <99404750+jorgemiguelabrito@users.noreply.github.com>
2025-10-21 11:22:01 -0300

    av25
    av25

--- db.js
+++ db.js
@@ -3,10 +3,9 @@ const { Pool } = require('pg');
 // Crie um objeto Pool para gerenciar as conexões com o banco
 const pool = new Pool({
     //
-    // SUA URL COMPLETA FOI COLADA AQUI
+    // Sua URL de conexão completa do Render
     // 
-    connectionString: 'dpg-d3ro5b95pdvs73fsa9cg-a.oregon-postgres.render.com',
-    //
+    connectionString: 'postgresql://aluguel_de_equipamentos_de_ti_user:LjGQVY0WmyohT8JMBGY4x57PIv2qjTy2@dpg-d3ro5b95pdvs73fsa9cg-a.oregon-postgres.render.com/aluguel_de_equipamentos_de_ti',
     
     // Configuração necessária para conexões SSL com o Render
     ssl: {
